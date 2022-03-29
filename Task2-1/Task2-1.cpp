72a909575a439e6f37baf174e6186b523ff5963f
Vadim Artemov <93925370+ElJohnSoap@users.noreply.github.com>
2022-03-29 08:14:10 +0300

    func delNullRows

---
+++ Task2-1/Task2-1.cpp
@@ -0,0 +1,104 @@
+﻿/*Задание 1. Написать программу, которая удаляет из
+двухмерного массива, заполненного случайными числами
+строки, содержащие нулевые элементы.*/
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int** creatArr(int rows, int cols);
+void initArr(int** Arr, int rows, int cols);
+void delArr(int** Arr, int rows);
+void printArr(int** Arr, int rows, int cols);
+void delNullCols(int**& Arr, int &rows, int cols);
+
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int rows = 5, cols = 5;
+    int** mArr = creatArr(rows, cols);
+    initArr(mArr, rows, cols);
+    printArr(mArr, rows, cols);
+    
+    delNullCols(mArr, rows, cols);
+
+    printArr(mArr, rows, cols);
+
+    return 0;
+}
+
+int** creatArr(int rows, int cols) {
+    int** tempArr = new int* [rows];
+    for (int i = 0; i < rows; i++) {
+        tempArr[i] = new int[cols];
+    }
+    return tempArr;
+}
+
+void initArr(int** Arr, int rows, int cols) {
+
+    srand(time(0));
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++){
+            Arr[i][j] = rand() % 10 - 5;
+        }
+    }
+}
+
+void delArr(int** Arr, int rows) {
+    for (int i = 0; i < rows; i++)
+    {
+        delete[] Arr[i];
+    }
+    delete[] Arr;
+}
+
+void printArr(int** Arr, int rows, int cols) {
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cout << Arr[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+}
+
+void delNullCols(int**& Arr, int &rows, int cols) {
+    int countRows = 0;
+    bool Z = false;//меняется при наличии 0
+    for (int i = 0; i < rows; i++) {
+        Z = false;
+        for (int j = 0; j < cols; j++) {
+            if (Arr[i][j] == 0) {
+                Z = true;
+                break;
+            }
+        }
+        if (!Z) countRows++;
+    }
+    cout << "Ноль есть в " << rows-countRows << " строчках\n\n";
+    if (!countRows) {
+        //cout << "В каждой строчке массива 0";
+        return void();//тут можно удалить массив по заданию. но не стал
+    }
+    int** tempArr = creatArr(countRows, cols);
+    for (int i = 0, i2=0; i < rows; i++, i2++) {
+        for (int j = 0; j < cols; j++) {
+            if (i2 < countRows) {
+                tempArr[i2][j] = Arr[i][j];
+                if (Arr[i][j] == 0) {
+                    i2--;
+                    break;
+                }
+            }
+        }
+    }
+    delArr(Arr, rows);
+    Arr = tempArr;
+    rows = countRows;
+}
---
+++ Task3/Task3.cpp
@@ -0,0 +1,26 @@
+﻿/*Задание 3. Дана матрица порядка M×N (M строк, N столбцов). Необходимо заполнить ее значениями и написать
+функцию, осуществляющую циклический сдвиг строк
+и/или столбцов массива указанное количество раз и в указанную сторону.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    int M, N;
+    cin >> M >> N;
+    int** ppArr = new int*[M];
+    for (int i = 0; i < M; i++) {
+        ppArr[i] = new int[N];
+    }
+    for (int i = 0, c = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            ppArr[i][j] = c;
+            c++;
+            cout << ppArr[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
